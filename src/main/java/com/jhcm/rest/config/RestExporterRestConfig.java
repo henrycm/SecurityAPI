57a5531b856a8735807a84f9c0bfbd763aef1b88
Henry Carmona <henry.carmona@clearwaterclinical.com>
2015-11-19 15:40:48 -0500

    Add gradle build

--- src/main/java/com/jhcm/rest/config/RestExporterRestConfig.java
+++ src/main/java/com/jhcm/rest/config/RestExporterRestConfig.java
@@ -1,7 +1,6 @@
 package com.jhcm.rest.config;
 
 import org.springframework.beans.factory.ObjectFactory;
-import org.springframework.boot.autoconfigure.data.rest.SpringBootRepositoryRestMvcConfiguration;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.repository.support.Repositories;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
@@ -17,38 +17,39 @@ import com.jhcm.rest.backend.model.User;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "generalAuditor")
-public class RestExporterRestConfig extends
-		SpringBootRepositoryRestMvcConfiguration {
+public class RestExporterRestConfig extends RepositoryRestConfigurerAdapter
+{
+    @Override
+    public void configureRepositoryRestConfiguration(
+        RepositoryRestConfiguration config )
+    {
+        config.exposeIdsFor( User.class );
+    }
 
-	@Override
-	protected void configureRepositoryRestConfiguration(
-			RepositoryRestConfiguration config) {
-		config.exposeIdsFor(User.class);
-	}
+    @Bean
+    public MessageSource messageSource()
+    {
+        ReloadableResourceBundleMessageSource bean = new ReloadableResourceBundleMessageSource();
+        bean.setBasename( "classpath:messages" );
+        bean.setDefaultEncoding( "UTF-8" );
+        return bean;
+    }
 
-	@Bean
-	public MessageSource messageSource() {
-		ReloadableResourceBundleMessageSource bean = new ReloadableResourceBundleMessageSource();
-		bean.setBasename("classpath:messages");
-		bean.setDefaultEncoding("UTF-8");
-		return bean;
-	}
+    @Bean
+    public Validator validator()
+    {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource( messageSource() );
+        return bean;
+    }
 
-	@Bean
-	public Validator validator() {
-		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
-		bean.setValidationMessageSource(messageSource());
-		return bean;
-	}
-
-	@Override
-	public ValidatingRepositoryEventListener validatingRepositoryEventListener(
-			ObjectFactory<Repositories> repositories) {
-		ValidatingRepositoryEventListener listener = new ValidatingRepositoryEventListener(
-				repositories);
-		configureValidatingRepositoryEventListener(listener);
-		listener.addValidator("beforeSave", validator());
-		return listener;
-	}
-
+    public ValidatingRepositoryEventListener validatingRepositoryEventListener(
+        ObjectFactory<Repositories> repositories )
+    {
+        ValidatingRepositoryEventListener listener = new ValidatingRepositoryEventListener(
+            repositories );
+        configureValidatingRepositoryEventListener( listener );
+        listener.addValidator( "beforeSave", validator() );
+        return listener;
+    }
 }
